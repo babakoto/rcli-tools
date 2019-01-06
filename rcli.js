1cce75e3efde7a99f017f6c7bb7aa829fc79e483
babakoto <e.tokiniaina@gmail.com>
2019-01-06 03:12:24 +0100

    add option stateless and statefull

--- rcli.js
+++ rcli.js
@@ -3,40 +3,104 @@ const fs = require('fs')
 const fse = require('fs-extra');
 
 const [,, ... args]=process.argv
-let template = 
-`
-import React, { Component } from 'react'
-import './${args}.css'
 
-class ${args} extends Component {
+let stateless=
+`import React from 'react';
+import './${args[2]}.css'
+
+const ${args[2]}=()=>(
+<h1>${args[2]}</h1>
+);
+
+export default ${args[2]}
+`
+
+let stateFull = 
+`import React, { Component } from 'react'
+import './${args[2]}.css'
+
+class ${args[2]} extends Component {
   render() {
     return (
       <div>
-        
+        ${args[2]}
       </div>
     )
   }
 }
 
-export default ${args}
+export default ${args[2]}
 `
+let exportDefault = `export {default} from "./${args[2]}"`;
 
-let exportFile = `export {default} from './${args}'`
-fse.outputFile(`src/${args[0]}/${args[1]}/${args[1]}.js`,template, err => {
-    if(err) {
-      console.log(err);
-    }
-  })
+let template = (dir,state)=>{
+  if (dir==="-cp"){
+    fse.outputFile(`src/components/${args[2]}/${args[2]}.js`,state, err => {
+      if(err) {
+        console.log(err);
+      }
+    })
+  
+    fse.outputFile(`src/components/${args[2]}/${args[2]}.css`,' ', err => {
+      if(err) {
+        console.log(err);
+      }
+    })
+  
+    fse.outputFile(`src/components/${args[2]}/index.js`, state, err => {
+      if(err) {
+        console.log(err);
+      }
+    })
+    console.log(`${args[2]} created !!!`)
 
-  fse.outputFile(`src/${args[0]}/${args[1]}/${args[1]}.css`,' ', err => {
-    if(err) {
-      console.log(err);
-    }
-  })
+  }else if (dir==="-ct"){
+    fse.outputFile(`src/containers/${args[2]}/${args[2]}.js`,state, err => {
+      if(err) {
+        console.log(err);
+      }
+    })
+  
+    fse.outputFile(`src/containers/${args[2]}/${args[2]}.css`,' ', err => {
+      if(err) {
+        console.log(err);
+      }
+    })
+  
+    fse.outputFile(`src/containers/${args[2]}/index.js`, state, err => {
+      if(err) {
+        console.log(err);
+      }
+    })
+    console.log(`${args[2]} created !!!`)
+  }else{
+    fse.outputFile(`src/${args[0]}/${args[2]}/${args[2]}.js`,state, err => {
+      if(err) {
+        console.log(err);
+      }
+    })
+  
+    fse.outputFile(`src/${args[0]}/${args[2]}/${args[2]}.css`,' ', err => {
+      if(err) {
+        console.log(err);
+      }
+    })
+  
+    fse.outputFile(`src/${args[0]}/${args[2]}/index.js`, state, err => {
+      if(err) {
+        console.log(err);
+      }
+    })
+    console.log("\x1b[32m",`${args[2]} created !!!`)
+  }
+}
 
-  fse.outputFile(`src/${args[0]}/${args[1]}/index.js`,exportFile, err => {
-    if(err) {
-      console.log(err);
-    }
-  })
-  console.log(`${args[1]} created !!!`)
+if(args[1]==="-l"){
+  template(args[0],stateless)
+}else if(args[1]==="-f"){
+  template(args[0],stateFull)
+}else{
+ 
+ console.log("\x1b[31m",`"${args[1]}" is not a rcli command`);
+}
+
